14e4ee3148d7870e9fcba7a1d95b7beb9effc997
AmandaDaria91 <34180620+AmandaDaria91@users.noreply.github.com>
2022-08-31 23:17:13 -0700

    Created prompts and game plan for code, worked on ReadMe

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # Password-Generator
-A password generator that creates a random password based on user selected criteria
+
+
+## Description
+
+A password generator that creates a random password based on user selected criteria. I made use of some begining code which was my HTML and CSS and created the use of the JavaScript Page. 
+
+- My motivation for this project was to take the work out of thinking of a passward and coming up with a unique and strong password based on user specified criteria.
+- I built this project because I believe that it will help provide users with stronger security.
+- This problem solves the issues users face to create strong and random passwords. 
+- I learned how to use JavaScript for my first time and apply it to a real world situation. 
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Badges](#badges)
+
+
+## Installation
+
+No installation or authentication required to view the webpage. 
+
+## Usage
+
+[Link to deployed site](https://www.github.com)
+
+![This is a screenshot of the password generator webpage.](assets/images/horiseon-screenshot-1.jpg)
+
+
+## Credits
+
+@UWA-Bootcamp provided starter code of both HTML and CSS, asset, and instruction into the foundational knowledge of HTML, CSS, and JavaScript. 
+
+
+## License
+
+I used an [MIT License](LICENSE).
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+
+
+
+
+
+
--- script.js
+++ script.js
@@ -1,6 +1,57 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//TODO: create an object to store the arrays for generate passwords to be generated from
+var options = {
+  specialCharOptions: ["!", "@", "#", "$", "%", "&", "*", ".", "_"],
+  letterOptions: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  numberOptions: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"], 
+}
+
+// TODO: generatePassword function
+function generatePassword () {
+  //prompt user for length of PW
+  var userChoiceLength = window.prompt("How long would you like your password to be?");
+  //verify user selects length between 8 and 128
+  if (userChoiceLength < 8 || 128 < userChoiceLength) {
+    window.alert("Only passwords between the 8 - 128 can be generated");
+    return;
+  }
+
+  //prompt user for special char inclusion using okay or cancel
+  var userChoiceSpecChar = window.confirm("Would you like special characters included?");
+
+  //prompt user for lower char inclusion using okay or cancel
+  var userChoiceLowerChar = window.confirm("Would you like lowercase letters included?");
+
+  //prompt user for upper char inclusion using okay or cancel
+  var userChoiceUpperChar = window.confirm("Would you like uppercase letters included?");
+
+  //prompt user for upper char inclusion using okay or cancel
+  var userChoiceNumber = window.confirm("Would you like numbers included?");
+
+  // TODO: if user selects Okay to Upper than use letterOptions array and change value to upper
+  if (userChoiceUpperChar) {
+    options.userChoiceUpperChar.upperCase();
+  }
+
+  //TODO: user must select one character type 
+  if (!userChoiceSpecChar || !userChoiceLowerChar || !userChoiceUpperChar || !userChoiceNumber) {
+    window.alert("You must select one character type");
+    return;
+  }
+
+  // //TODO: for loop the amount of times for the length specified to randomly select between the 3 arrays
+  // for (var i = 0; i =< userChoiceLength.length; i++) {
+  //   var randomPass = specialCharOptions[Math.floor(Math.random() * userChoiceSpecChar)];
+  //   var randomPass = userChoiceLowerChar[Math.floor(Math.random() * userChoiceLowerChar)];
+  //   var randomPass = userChoiceUpperChar[Math.floor(Math.random() * userChoiceUpperChar)];
+  //   var randomPass = Math.floor(Math.random() * userChoiceNumber);
+  // }
+  // return randomPass;
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +61,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
