e4d533d11296203b61494fa3c9eb3f7fb95e288b
AmandaDaria91 <34180620+AmandaDaria91@users.noreply.github.com>
2022-09-01 22:34:33 -0700

    removed TODO from comments

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ var  letterOptionsLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"
 var  letterOptionsUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var  numberOptions = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
 
-// TODO: empty array for possible characters
+// empty array for possible characters
 let possibleCharOptions = [];
 
-// TODO: generatePassword function
+// generatePassword function
 function generatePassword () {
   //prompt user for length of PW
   var userChoiceLength = parseInt(prompt("How long would you like your password to be?"));
@@ -38,33 +38,33 @@ function generatePassword () {
   //prompt user for upper char inclusion using okay or cancel
   var userChoiceNumber = confirm("Would you like numbers included?");
 
-  //TODO: user must select one character type 
+  //user must select one character type 
   if (!userChoiceSpecChar && !userChoiceLowerChar && !userChoiceUpperChar && !userChoiceNumber) {
     alert("You must select one character type");
     return;
   }
 
-  // TODO: if user selects Okay to special characters than use specialCharOptions array and add elements into empty array
+  // if user selects Okay to special characters than use specialCharOptions array and add elements into empty array
   if (userChoiceSpecChar) {
     possibleCharOptions = possibleCharOptions.concat(specialCharOptions);
   }
 
-  // TODO: if user selects Okay to lower than use letterOptions array and add elements into empty array
+  // if user selects Okay to lower than use letterOptions array and add elements into empty array
   if (userChoiceLowerChar) {
     possibleCharOptions = possibleCharOptions.concat(letterOptionsLower);
   }
 
-  // TODO: if user selects Okay to Upper than use letterOptions array and change value to upper and add elements into empty array
+  //if user selects Okay to Upper than use letterOptions array and change value to upper and add elements into empty array
   if (userChoiceUpperChar) {
     possibleCharOptions = possibleCharOptions.concat(letterOptionsUpper);
   }
     
-  // TODO: if user selects Okay to special characters than use specialCharOptions array and add elements into empty array
+  // if user selects Okay to special characters than use specialCharOptions array and add elements into empty array
   if (userChoiceNumber) {
     possibleCharOptions = possibleCharOptions.concat(numberOptions);
   }
 
-  // //TODO: for loop the amount of times for the length specified by usern to randomly select between the 3 arrays
+  // //for loop the amount of times for the length specified by usern to randomly select between the 3 arrays
   let result = [];
   for (var i = 0; i < userChoiceLength; i++) {  
     var randomChar = possibleCharOptions[Math.floor(Math.random() * possibleCharOptions.length)];
